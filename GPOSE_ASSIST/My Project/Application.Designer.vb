6467dd68169b6c3f7836815aeffa9b27ee9b3260
DenPaKousya <denpakousya@gmail.com>
2021-01-27 11:38:42 +0900

    オーディオリソースの追加

--- GPOSE_ASSIST/My Project/Application.Designer.vb
+++ GPOSE_ASSIST/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GPOSE_ASSIST.Form1
+            Me.MainForm = Global.GPOSE_ASSIST.FRM_MAIN
         End Sub
     End Class
 End Namespace
