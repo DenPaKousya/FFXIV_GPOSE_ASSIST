6afdf097d893bce050196f49367b5d6b0a5b2ef2
DenPaKousya <denpakousya@gmail.com>
2021-01-28 14:00:45 +0900

    設定保存の追加

--- GPOSE_ASSIST/MOD/MOD_CONFIG_TOOL.vb
+++ GPOSE_ASSIST/MOD/MOD_CONFIG_TOOL.vb
@@ -21,6 +21,15 @@
     End Structure
 #End Region
 
+#Region "モジュール用・定数"
+    Private Const cstCONFIG_EXTENT As String = "config"
+    Private Const cstCONFIE_KEY_MAIN As String = "configuration"
+    Private Const cstCONFIE_KEY_SUB As String = "appSettings"
+    Private Const cstCONFIG_KEY_ATTRIBUTE_ADD As String = "add"
+    Private Const cstCONFIG_KEY_PROPERTY_KEY As String = "key"
+    Private Const cstCONFIG_KEY_PROPERTY_VALUE As String = "value"
+#End Region
+
     Public Function FUNC_GET_APP_SETTINGS(ByVal STR_KEY As String) As String
         Dim OBJ_TEMP As Object
         Dim STR_RET As String
@@ -38,6 +47,63 @@
         STR_RET = OBJ_TEMP.ToString
 
         Return STR_RET
+    End Function
+
+    Public Function FUNC_WRITE_APP_SETTINGS(ByVal strKEY_NAME As String, ByVal strVALUE As String)
+
+        Dim STR_NAME_EXE As String
+        STR_NAME_EXE = System.IO.Path.GetFileName(System.Windows.Forms.Application.ExecutablePath)
+
+        Dim STR_NAME_APPL As String
+        STR_NAME_APPL = STR_NAME_EXE.Replace(".exe", "")
+
+        Dim STR_NAME_DLL As String
+        STR_NAME_DLL = STR_NAME_APPL & ".dll"
+
+        Dim ASM_EXEC As System.Reflection.Assembly
+        ASM_EXEC = System.Reflection.Assembly.GetExecutingAssembly()
+
+        Dim STR_PATH_CONFIG_APP As String
+        STR_PATH_CONFIG_APP = System.IO.Path.GetDirectoryName(ASM_EXEC.Location) & "\" & STR_NAME_DLL & "." & cstCONFIG_EXTENT
+
+        Dim doc As System.Xml.XmlDocument
+        doc = New System.Xml.XmlDocument
+
+        Try
+            Call doc.Load(STR_PATH_CONFIG_APP)
+        Catch ex As Exception
+            doc = Nothing
+            Return False
+        End Try
+
+        Dim node As System.Xml.XmlNode
+        node = doc(cstCONFIE_KEY_MAIN)(cstCONFIE_KEY_SUB)
+
+        Dim blnMOD As Boolean
+        blnMOD = False
+        For Each n In doc(cstCONFIE_KEY_MAIN)(cstCONFIE_KEY_SUB)
+            If n.Name = cstCONFIG_KEY_ATTRIBUTE_ADD Then
+                If n.Attributes.GetNamedItem(cstCONFIG_KEY_PROPERTY_KEY).Value = strKEY_NAME Then
+                    n.Attributes.GetNamedItem(cstCONFIG_KEY_PROPERTY_VALUE).Value = strVALUE
+                    blnMOD = True
+                End If
+            End If
+        Next
+
+        If Not blnMOD Then
+            Dim newNode As System.Xml.XmlElement
+            newNode = doc.CreateElement(cstCONFIG_KEY_ATTRIBUTE_ADD)
+            newNode.SetAttribute(cstCONFIG_KEY_PROPERTY_KEY, strKEY_NAME)
+            newNode.SetAttribute(cstCONFIG_KEY_PROPERTY_VALUE, strVALUE)
+            node.AppendChild(newNode)
+        End If
+
+        doc.Save(STR_PATH_CONFIG_APP)
+        doc = Nothing
+
+        Call System.Configuration.ConfigurationManager.RefreshSection(cstCONFIE_KEY_SUB)
+
+        Return True
     End Function
 
 End Module
--- GPOSE_ASSIST/MOD/MOD_FIN.vb
+++ GPOSE_ASSIST/MOD/MOD_FIN.vb
@@ -2,5 +2,30 @@
 
     Public Sub SUB_APPL_FIN()
         Call SUB_END_GAPHICS()
+
+        Call FUNC_SET_APP_SETTING()
     End Sub
+
+    Private Function FUNC_SET_APP_SETTING() As Boolean
+        Dim STR_TEMP As String
+
+        With SRT_APP_SETTINGS_VALUE
+            Try
+                STR_TEMP = CStr(.PROCESS_NAME)
+                Call FUNC_WRITE_APP_SETTINGS(CST_APP_CONFIG_PROCESS_NAME, STR_TEMP)
+
+                'SAVE<
+
+                'SAVE.FILE<
+                STR_TEMP = CStr(.SAVE.FILE.INDEX)
+                Call FUNC_WRITE_APP_SETTINGS(CST_APP_CONFIG_SAVE_FILE_INDEX, STR_TEMP)
+                '>SAVE.FILE
+                '>SAVE
+            Catch ex As Exception
+                Return False
+            End Try
+        End With
+
+        Return True
+    End Function
 End Module
