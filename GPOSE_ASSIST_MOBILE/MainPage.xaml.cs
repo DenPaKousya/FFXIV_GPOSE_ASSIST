8820cfc6abbca08f4d9cb30cf9e8cafd1cad2c8a
電波 公社 <denpakousya@gmail.com>
2025-03-26 18:17:37 +0900

    警告訂正

--- GPOSE_ASSIST_MOBILE/MainPage.xaml.cs
+++ GPOSE_ASSIST_MOBILE/MainPage.xaml.cs
@@ -2,14 +2,10 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
-
-
-
         }
 
         private void Layout_Loaded(object sender, EventArgs e)
@@ -36,20 +32,6 @@
 #else
             App.Current?.Quit();
 #endif
-        }
-
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            //count++;
-            //DisplayAlert("Alert", "You have been alerted", "OK");
-            //if (count == 1)
-            //    CounterBtn.Text = $"Clicked {count} time";
-            //else
-            //    CounterBtn.Text = $"Clicked {count} times";
-
-            //GPOSE_ASSIST_LIB.MOD_NETWORK_TCP.FUNC_CLIENT_SEND(count.ToString());
-            //SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
         private void BTN_Clicked(object sender, EventArgs e)
